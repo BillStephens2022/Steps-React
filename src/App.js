6e1dbcf1cd693e56cfdc6fb3007de1258173495f
BillStephens2022 <two4onebill@yahoo.com>
2024-07-06 07:34:09 -0400

    added functionality to show & hide the app, added app screenshot

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handlePrevClick = () => {
     if (step > 1) setStep((prevStep) => prevStep - 1);
@@ -19,31 +20,46 @@ export default function App() {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          disabled={step === 1}
-          style={{ backgroundColor: step===1 ? "gray" : "#7950f2", color: "#fff", cursor: step===1 ? "not-allowed" : "pointer"}}
-          onClick={handlePrevClick}
-        >
-          Previous
-        </button>
-        <button
-          disabled={step === 3}
-          style={{ backgroundColor: step===3 ? "gray" : "#7950f2", color: "#fff", cursor: step===3 ? "not-allowed" : "pointer" }}
-          onClick={handleNextClick}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              disabled={step === 1}
+              style={{
+                backgroundColor: step === 1 ? "gray" : "#7950f2",
+                color: "#fff",
+                cursor: step === 1 ? "not-allowed" : "pointer",
+              }}
+              onClick={handlePrevClick}
+            >
+              Previous
+            </button>
+            <button
+              disabled={step === 3}
+              style={{
+                backgroundColor: step === 3 ? "gray" : "#7950f2",
+                color: "#fff",
+                cursor: step === 3 ? "not-allowed" : "pointer",
+              }}
+              onClick={handleNextClick}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
